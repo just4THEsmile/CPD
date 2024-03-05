73063a4557eefbba0dda392d178c9cb9cbf41bbb
justauser <diogosarmento4@gmail.com>
2024-03-05 10:13:29 +0000

    gcctester

---
+++ assign1/src/gcctester.py
@@ -0,0 +1,26 @@
+import os
+os.system("./fileout")
+##testing the mult normal to values
+values1=[600,3000]
+for i in range(600,3001,400):
+    os.system("1")
+    os.system(str(i))
+
+for i in range(600,3001,400):
+    os.system("2")
+    os.system(str(i))
+
+for i in range(4096,10241,2048):
+    os.system("2")
+    os.system(str(i))
+
+for i in range(4096,10241,2048):
+    os.system("3")
+    os.system("128")
+    os.system(str(i))
+    os.system("3")
+    os.system("256")
+    os.system(str(i))  
+    os.system("3")  
+    os.system("512")
+    os.system(str(i))
